a78351dccc9ca606ac3995f39715791891ecb148
Kevin Zolkiewicz <kevin@zolk.com>
2021-12-29 17:04:08 -0600

    Add support for collapsing parent nav items

---
+++ docs/plugins/navbar-collapse.js
@@ -0,0 +1,31 @@
+function docsifyNavbarCollapse(hook) {
+  hook.doneEach(() => {
+    const navbarItems = Array.from(
+      document.querySelectorAll('main > .sidebar > .sidebar-nav > ul > li')
+    );
+
+    navbarItems.forEach((item) => {
+      // Determine if nav item has sub menu
+      const submenu = item.querySelector('ul');
+      if (submenu) {
+        item.classList.add('has-submenu');
+
+        // Allow clicking on an active parent to collapse the sub menu
+        const itemLink = item.querySelector('a');
+        itemLink.addEventListener('click', () => {
+          item.classList.toggle('collapse');
+        });
+      }
+
+      // Determine if nav item has an active child
+      const activeChild = item.querySelector('li.active');
+      if (activeChild) {
+        item.classList.add('active-child');
+        activeChild.classList.add('active-child');
+      }
+    });
+  });
+}
+
+window.$docsify = window.$docsify || {};
+window.$docsify.plugins = [docsifyNavbarCollapse].concat(window.$docsify.plugins || []);
