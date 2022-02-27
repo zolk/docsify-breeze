d23c4103e6c12eb96608660497c5b35a4bac09e8
Kevin Zolkiewicz <kevin@zolk.com>
2022-02-27 09:51:26 -0600

    don't bother with plugin minification

--- docs/assets/plugins/minified/fix-pagination.js
+++
@@ -1,8 +0,0 @@
-window.$docsify.plugins.push((o) => {
-  o.doneEach(function () {
-    const n = document.querySelector('.content > .docsify-pagination-container'),
-      e = document.querySelector('.docsify-pagination-container'),
-      t = document.querySelector('.content');
-    n && n.remove(), t.appendChild(e);
-  });
-});
