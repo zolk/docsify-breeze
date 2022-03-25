fc4f4abec4c6b259a60a15f9e82b5c84e1495ffd
Kevin Zolkiewicz <kevin@zolk.com>
2022-03-25 18:29:19 -0500

    with the file

---
+++ snapshots.cjs
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async () => {
+  let snapshots = [];
+
+  const { globby } = await import('globby');
+  const components = await globby('./docs/components/*.md');
+
+  function getSlugs(markdown) {
+    let slugs = [];
+    const lines = markdown.split('\n');
+
+    lines.map((line) => {
+      if (line.startsWith('```html preview')) {
+        const slug = line.match(/\b(\w+)$/g);
+        slugs.push(slug);
+      }
+    });
+
+    return slugs;
+  }
+
+  components.map((file) => {
+    const content = fs.readFileSync(file, 'utf8');
+    const slugs = getSlugs(content);
+    const filename = path.basename(file, '.md');
+
+    slugs.map((slug) => {
+      const url = `http://localhost:3000/components/${filename}?example=${slug}`;
+      snapshots.push({ name: `${filename}/${slug}`, url: url });
+    });
+  });
+
+  return snapshots.map((snapshot) => ({ name: snapshot.name, url: snapshot.url }));
+};
