ad5af3a6c0819507424005e8a2648b788deba1da
Kevin Zolkiewicz <kevin@zolk.com>
2022-09-22 17:59:31 -0500

    refactor component plugin

---
+++ docs/assets/plugins/component/lib/metadata.js
@@ -0,0 +1,239 @@
+/*
+ * Copyright (c) 2021 Kevin Zolkiewicz.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ * This code builds upon a similar plugin developed for use with Shoelace <https://shoelace.style/>.
+ * Copyright (c) 2020 A Beautiful Site, LLC
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+function renderPropertiesTable(props) {
+  return `
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Property</th>
+            <th scope="col">Attribute</th>
+            <th scope="col">Description</th>
+            <th scope="col">Type</th>
+            <th scope="col">Default</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${props
+            .map((prop) => {
+              return `<tr>
+                <th scope="row"><code>${prop.name}</code></th>
+                <td><code>${prop.attribute}</code></td>
+                <td>${prop.description.replace(/`(.*?)`/g, '<code>$1</code>')}</td>
+                <td><code>${prop.type.text.replace(/^\| /m, '')}</code></td>
+                <td>${prop.default ? `<code>${prop.default}</code>` : '&ndash;'}</td>
+              </tr>`;
+            })
+            .join('')}
+        </tbody>
+      </table>
+    `;
+}
+
+function renderMethodsList(methods) {
+  let result = '';
+
+  methods.map((method) => {
+    const hasParams = method.parameters?.length;
+
+    result += `
+        <h4 class="component-method-signature"><code>${method.name}(${
+      hasParams
+        ? method.parameters
+            .map((param) => `${param.name}${param.optional ? '?' : ''}: ${param.type.text}`)
+            .join(', ')
+        : ''
+    }) => ${method.return ? method.return.type.text : 'void'}</code></h4>
+        <p>${method.description}</p>
+
+        ${
+          hasParams
+            ? `
+              <dl class="component-method-options">
+              ${method.parameters.map((param) =>
+                param.description
+                  ? `<dt><code>${param.name}</code></dt><dd>${param.description}</dd>`
+                  : ``
+              )}
+              </dl>
+              `
+            : ``
+        }
+      `;
+  });
+
+  return result;
+}
+
+function renderEventsTable(events) {
+  return `
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Event Detail</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${events
+            .map((event) => {
+              return `
+                <tr>
+                  <th scope="row">
+                    <code>${event.name}</code>
+                  </th>
+                  <td>${event.description}</td>
+                  <td>${event.type?.text ? `<code>${event.type?.text}</code>` : '-'}</td>
+                </tr>
+              `;
+            })
+            .join('')}
+        </tbody>
+      </table>
+    `;
+}
+
+function renderSlotsTable(slots) {
+  return `
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${slots
+            .map((slot) => {
+              return `
+                <tr>
+                  <th scope="row">
+                    ${slot.name === '' ? 'Default slot' : `<code>${slot.name}</code>`}
+                  </th>
+                  <td>${slot.description}</td>
+                </tr>
+              `;
+            })
+            .join('')}
+        </tbody>
+      </table>
+    `;
+}
+
+function renderCssPartsTable(parts) {
+  return `
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${parts
+            .map((part) => {
+              return `
+                <tr>
+                  <th scope="row"><code>${part.name}</code></th>
+                  <td>${part.description}</td>
+                </tr>
+              `;
+            })
+            .join('')}
+        </tbody>
+      </table>
+    `;
+}
+
+function renderCssPropertiesTable(properties) {
+  return `
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${properties
+            .map((property) => {
+              return `
+                <tr>
+                  <th scope="row"><code>${property.name}</code></th>
+                  <td>${property.description}</td>
+                </tr>
+              `;
+            })
+            .join('')}
+        </tbody>
+      </table>
+    `;
+}
+
+export function renderMetadata(metadata) {
+  let result = '';
+
+  const members = metadata.members?.filter(
+    (member) => member.description && member.privacy !== 'private'
+  );
+  const props = members?.filter((prop) => {
+    return prop.kind === 'field';
+  });
+  const methods = members?.filter(
+    (member) => member.kind === 'method' && member.privacy !== 'private'
+  );
+
+  if (props?.length) {
+    result += `
+          ### Properties
+          ${renderPropertiesTable(props)}
+        `;
+  }
+
+  if (methods?.length) {
+    result += `
+          ### Methods
+          ${renderMethodsList(methods)}
+        `;
+  }
+
+  if (metadata.events?.length) {
+    result += `
+          ### Events
+          ${renderEventsTable(metadata.events)}
+        `;
+  }
+
+  if (metadata.slots?.length) {
+    result += `
+          ### Slots
+          ${renderSlotsTable(metadata.slots)}
+        `;
+  }
+
+  if (metadata.cssParts?.length) {
+    result += `
+          ### CSS Parts
+          ${renderCssPartsTable(metadata.cssParts)}
+        `;
+  }
+
+  if (metadata.cssProperties?.length) {
+    result += `
+          ### CSS Custom Properties
+          ${renderCssPropertiesTable(metadata.cssProperties)}
+        `;
+  }
+
+  return result;
+}
