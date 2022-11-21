84463fab6c01c01e05233acd49f2642824b292c9
Kevin Zolkiewicz <kevin@zolk.com>
2022-11-21 16:34:07 -0600

    switch to playwright for testing

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,5 +1,14 @@
 import { esbuildPlugin } from "@web/dev-server-esbuild";
+import { playwrightLauncher } from "@web/test-runner-playwright";
 
 export default {
+  files: "src/**/*.test.ts",
+  concurrentBrowsers: 3,
+  nodeResolve: true,
+  browsers: [
+    playwrightLauncher({ product: "chromium" }),
+    playwrightLauncher({ product: "firefox" }),
+    playwrightLauncher({ product: "webkit" }),
+  ],
   plugins: [esbuildPlugin({ ts: true, target: "auto" })],
 };
