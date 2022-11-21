bc3396f1731b75febbdcaf4c0b163fa2c7834bae
Kevin Zolkiewicz <kevin@zolk.com>
2022-11-21 16:22:45 -0600

    update plopfile formatting

--- plopfile.cjs
+++ plopfile.cjs
@@ -1,24 +1,24 @@
 module.exports = function (plop) {
-  plop.setHelper('noPrefix', (tag) => tag.replace(/^ds-/, ''));
+  plop.setHelper("noPrefix", (tag) => tag.replace(/^ds-/, ""));
 
-  plop.setHelper('tagToTitle', (tag) => {
-    const withoutPrefix = plop.getHelper('noPrefix');
-    const titleCase = plop.getHelper('titleCase');
+  plop.setHelper("tagToTitle", (tag) => {
+    const withoutPrefix = plop.getHelper("noPrefix");
+    const titleCase = plop.getHelper("titleCase");
     return titleCase(withoutPrefix(tag));
   });
 
-  plop.setHelper('tagToDsTitle', (tag) => {
-    const tagToTitle = plop.getHelper('tagToTitle');
-    return 'Ds' + tagToTitle(tag);
+  plop.setHelper("tagToDsTitle", (tag) => {
+    const tagToTitle = plop.getHelper("tagToTitle");
+    return "Ds" + tagToTitle(tag);
   });
 
-  plop.setGenerator('component', {
-    description: 'create files for a new component',
+  plop.setGenerator("component", {
+    description: "create files for a new component",
     prompts: [
       {
-        type: 'input',
-        name: 'tag',
-        message: 'tag name (e.g. ds-button)',
+        type: "input",
+        name: "tag",
+        message: "tag name (e.g. ds-button)",
         validate: (value) => {
           // Starts with ds- and include only a-z + dashes
           if (!/^ds-[a-z-+]+/.test(value)) {
@@ -31,34 +31,34 @@ module.exports = function (plop) {
     ],
     actions: [
       {
-        type: 'add',
-        path: 'src/components/{{name}}/{{name}}.ts',
-        templateFile: 'scripts/plop/templates/component.hbs',
+        type: "add",
+        path: "src/components/{{name}}/{{name}}.ts",
+        templateFile: "scripts/plop/templates/component.hbs",
       },
       {
-        type: 'add',
-        path: 'src/components/{{name}}/{{name}}.styles.ts',
-        templateFile: 'scripts/plop/templates/component.styles.hbs',
+        type: "add",
+        path: "src/components/{{name}}/{{name}}.styles.ts",
+        templateFile: "scripts/plop/templates/component.styles.hbs",
       },
       {
-        type: 'add',
-        path: 'src/components/{{name}}/{{name}}.test.ts',
-        templateFile: 'scripts/plop/templates/component.test.hbs',
+        type: "add",
+        path: "src/components/{{name}}/{{name}}.test.ts",
+        templateFile: "scripts/plop/templates/component.test.hbs",
       },
       {
-        type: 'add',
-        path: 'docs/components/{{name}}/{{name}}.md',
-        templateFile: 'scripts/plop/templates/component.docs.hbs',
+        type: "add",
+        path: "docs/components/{{name}}/{{name}}.md",
+        templateFile: "scripts/plop/templates/component.docs.hbs",
       },
       {
-        type: 'modify',
-        path: 'src/index.ts',
+        type: "modify",
+        path: "src/index.ts",
         pattern: /\/\* plop:component \*\//,
         template: `export { default as Ds{{ properCase name }} } from './components/{{ name }}/{{ name }}';\n/* plop:component */`,
       },
       {
-        type: 'modify',
-        path: 'docs/_sidebar.md',
+        type: "modify",
+        path: "docs/_sidebar.md",
         pattern: /<!--plop:component-->/,
         template: `- [{{ properCase name }}](/components/code)\n  <!--plop:component-->`,
       },
