83dd5cc2c912a14a4e3dce563b847fa4a3715e60
Kevin Zolkiewicz <kevin@zolk.com>
2022-11-27 17:01:07 -0600

    Prep web component viewer for extraction

--- docs/assets/plugins/web-component-viewer/lib/preview.js
+++ docs/assets/plugins/web-component-viewer/lib/preview.js
@@ -4,6 +4,8 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+import { controlsIcon, windowIcon, resizerIcon, arrowIcon } from "./icons.js";
+
 export function renderPreview(codeBlock, id) {
   const pre = codeBlock.closest("pre");
   const isExpanded = codeBlock.classList.contains("expanded");
@@ -16,33 +18,27 @@ export function renderPreview(codeBlock, id) {
   const getSlug = () => (hasSlug ? lastClass : id);
   const exampleId = "example-" + getSlug();
 
+  // Controls button
   const controlsBtn = `
     <div class="code-preview__controls">
       <a href="?controls=${getSlug()}" target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path
-            d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"
-          />
-        </svg>
+        ${controlsIcon}
         Customize
       </a>
     </div>
   `;
 
+  // Preview button
   const previewBtn = `
     <div class="code-preview__new-window">
       <a href="?preview=${getSlug()}" target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 448 512">
-          <path
-            d="M256 64c0-17.67 14.3-32 32-32h127.1c5.2 0 9.4.86 13.1 2.43 2.9 1.55 7.3 3.84 10.4 6.87 0 .05 0 .1.1.14 6.2 6.22 8.4 14.34 9.3 22.46V192c0 17.7-14.3 32-32 32s-32-14.3-32-32v-50.7L214.6 310.6c-12.5 12.5-32.7 12.5-45.2 0s-12.5-32.7 0-45.2L338.7 96H288c-17.7 0-32-14.33-32-32zM0 128c0-35.35 28.65-64 64-64h96c17.7 0 32 14.33 32 32 0 17.7-14.3 32-32 32H64v288h288v-96c0-17.7 14.3-32 32-32s32 14.3 32 32v96c0 35.3-28.7 64-64 64H64c-35.35 0-64-28.7-64-64V128z"
-            fill="currentColor"
-          />
-        </svg>
+        ${windowIcon}
         Open in New Window
       </a>
     </div>
   `;
 
+  // Preview UI
   const previewInterface = `
     <div class="code-preview ${isExpanded ? "code-preview--expanded" : ""}">
       <div class="code-preview__preview" id=${exampleId}>
@@ -53,7 +49,7 @@ export function renderPreview(codeBlock, id) {
           role="slider"
           tabindex="0"
         >
-          <svg width="11" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M.586 3.414A2 2 0 1 0 3.414.586 2 2 0 0 0 .586 3.414ZM.586 9.414a2 2 0 1 0 2.828-2.828A2 2 0 0 0 .586 9.414ZM.586 15.414a2 2 0 1 0 2.828-2.828 2 2 0 0 0-2.828 2.828ZM7.586 3.414A2 2 0 1 0 10.414.586a2 2 0 0 0-2.828 2.828ZM7.586 9.414a2 2 0 1 0 2.828-2.828 2 2 0 0 0-2.828 2.828ZM7.586 15.414a2 2 0 1 0 2.828-2.828 2 2 0 0 0-2.828 2.828Z" fill="currentColor"/></svg>
+          ${resizerIcon}
         </div>
       </div>
       <div class="code-preview__source" id=${sourceId}>${pre.outerHTML}</div>
@@ -63,9 +59,7 @@ export function renderPreview(codeBlock, id) {
           aria-expanded="${isExpanded ? "true" : "false"}"
           aria-controls="${sourceId}"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-          </svg>
+          ${arrowIcon}
           <span>
             ${isExpanded ? "Hide " : "Show"} Code
           </span>
